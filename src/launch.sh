973902d3a56b03a4a915408006302954a9a806df
Ankush <51945739+ankush13r@users.noreply.github.com>
2024-07-03 11:35:10 +0200

    Update launch.sh

--- src/launch.sh
+++ src/launch.sh
@@ -8,6 +8,8 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --qos=acc_bscls
 #SBATCH --partition=acc
+#SBATCH --nodes=1
+#SBATCH --exclusive
 
 echo "Launch python script"
 singularity exec --nv --no-home --pwd /src --bind $JOB_PATH:/src --bind /gpfs:/gpfs $GPFS_SINGULARITY_IMAGE_REGISTRY_PATH/python-jupyter.sif python code/script.py > $JOB_LOGS_PATH/script.log 2>&1
