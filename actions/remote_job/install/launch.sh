1725affa87a2f57a6ed487097fcc1f9a1f8acbd9
Ankush Rana <ankush.rana@bsc.es>
2024-07-08 12:47:38 +0200

    WIp

--- actions/remote_job/install/launch.sh
+++ actions/remote_job/install/launch.sh
@@ -9,66 +9,60 @@
 #SBATCH --partition=gpp
 #SBATCH --nodes=1
 
-source $JOB_PATH/remote_job.env
+# Load environment variables and modules
+source "$JOB_PATH/remote_job.env"
 module load singularity
 
-IMAGES_PATH=$REMOTE_JOB_PATH
-IMAGE=$REMOTE_JOB_IMAGE
-OLD_IMAGE=$REMOTE_JOB_OLD_IMAGE
-REQUIREMENTS_PATH=$REMOTE_JOB_REQUIREMENTS_PATH
-TMP_IMAGE=$(echo $REMOTE_JOB_IMAGE | sed 's/\//_/g')
-TMP_IMAGE=${TMP_IMAGE}_sandbox
+# Define paths and variables
+IMAGES_PATH="$REMOTE_JOB_PATH"
+IMAGE="$REMOTE_JOB_IMAGE"
+OLD_IMAGE="$REMOTE_JOB_OLD_IMAGE"
+REQUIREMENTS_PATH="$REMOTE_JOB_REQUIREMENTS_PATH"
+TMP_IMAGE="${REMOTE_JOB_IMAGE//\//_}_sandbox"
 
-echo "IMAGES_PATH: $IMAGES_PATH"
-echo "IMAGE: $IMAGE"
-echo "OLD_IMAGE: $OLD_IMAGE"
-echo "REQUIREMENTS_PATH: $REQUIREMENTS_PATH"
-
-# mkdir -p $IMAGES_PATH
-
-
-if [ -f $IMAGES_PATH/$OLD_IMAGE ]; then
+# Create or move the sandbox image
+if [ -f "$IMAGES_PATH/$OLD_IMAGE" ]; then
     echo "Build sandbox from singularity"
-    SANDBOX="true"
-    singularity build --sandbox $TMP_IMAGE $IMAGES_PATH/$OLD_IMAGE
-elif [ -d $IMAGES_PATH/$OLD_IMAGE ]; then
-    echo "Move sandbox"
-    cp -r $IMAGES_PATH/$OLD_IMAGE $TMP_IMAGE
+    singularity build --sandbox "$TMP_IMAGE" "$IMAGES_PATH/$OLD_IMAGE"
+elif [ -d "$IMAGES_PATH/$OLD_IMAGE" ]; then
+    echo "Copy sandbox"
+    cp -r "$IMAGES_PATH/$OLD_IMAGE" "$TMP_IMAGE"
 else
     echo "Not a sandbox or singularity"
     exit 1
 fi
 
-echo "Variables
-echo "\tIMAGES_PATH: $IMAGES_PATH"
-echo "\tIMAGE: $IMAGE"
-echo "\tREQUIREMENTS_PATH: $REQUIREMENTS_PATH"
-echo "\tSANDBOX: $SANDBOX"
-echo "\tTMP_IMAGE: $TMP_IMAGE"
+# Display variables
+cat <<EOF
+Variables:
+    IMAGES_PATH: $IMAGES_PATH
+    IMAGE: $IMAGE
+    REQUIREMENTS_PATH: $REQUIREMENTS_PATH
+    TMP_IMAGE: $TMP_IMAGE
+EOF
 
-echo ""
-echo "Create requirementes dir"
-singularity exec --contain -w --no-home $TMP_IMAGE /bin/sh -c 'mkdir -p /requirements'
+# Create requirements directory and install dependencies
+singularity exec --contain -w --no-home "$TMP_IMAGE" /bin/sh -c 'mkdir -p /requirements'
+singularity exec --contain -w --no-home --bind "$REQUIREMENTS_PATH/requirements:/requirements" "$TMP_IMAGE" /bin/sh -c 'export TMPDIR=/tmp && pip install -r /requirements/toinstall.txt --no-index --force-reinstall --find-links=/requirements/'
 
-echo "Install dependecies"
-singularity exec -w --contain --no-home --bind $REQUIREMENTS_PATH/requirements:/requirements $TMP_IMAGE /bin/sh -c  'export TMPDIR=/tmp && pip install --force-reinstall /requirements/*'
 
-if [ "$SANDBOX" = "true" ]; then
-    echo "Build singularity from sandbox"
-
-    NEW_NAME=${TMP_IMAGE}.sif
-    singularity build --force $NEW_NAME $TMP_IMAGE
-    rm -rf $TMP_IMAGE
-    TMP_IMAGE=$NEW_NAME
-fi
-
-touch $JOB_LOGS_PATH/error.log
+# Check for errors in the log file
 if grep -q "^ERROR:" "$JOB_LOGS_PATH/error.log"; then
     echo "An error was detected"
     grep "^ERROR:" "$JOB_LOGS_PATH/error.log"
+    rm -rf $TMP_IMAGE
     exit 1
 fi
 
-echo "Move singularity with new image name"
-mv $TMP_IMAGE $IMAGES_PATH/$IMAGE
-rm -r $REQUIREMENTS_PATH/requirements
+# Convert the sandbox to a SIF file if it was built
+if [ -f "$IMAGES_PATH/$OLD_IMAGE" ]; then
+    echo "Build singularity from sandbox"
+    NEW_NAME="${TMP_IMAGE}.sif"
+    singularity build --force "$NEW_NAME" "$TMP_IMAGE"
+    rm -rf "$TMP_IMAGE"
+    TMP_IMAGE="$NEW_NAME"
+fi
+
+# Move the new image to the images path
+mv "$TMP_IMAGE" "$IMAGES_PATH/$IMAGE"
+# rm -rf "$REQUIREMENTS_PATH/requirements"
