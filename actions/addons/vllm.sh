f6bb36f9497271aa6148022354c15deb75eb8f8e
Ankush Rana <ankush.rana@bsc.es>
2024-07-23 16:37:38 +0200

    finaly working all

--- actions/addons/vllm.sh
+++ actions/addons/vllm.sh
@@ -4,4 +4,4 @@ nohup singularity run --nv \
     $GPFS_VLLM_SINGULARITY  \
     --model /data/$GPFS_VLLM_MODEL \
     --host 0.0.0.0 \
-    --port 8080 $(echo $JOB_VLLM_PARAMS) > $JOB_PATH/logs/vllm.log 2>&1
+    --port 8080 $(echo $JOB_VLLM_PARAMS) > $JOB_PATH/logs/vllm.log 2>&1 &
