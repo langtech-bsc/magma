6aac182e4230475ee825854aae7737644843ece7
Ankush Rana <ankush.rana@bsc.es>
2024-07-23 15:56:41 +0200

    big changes

---
+++ actions/addons/vllm.sh
@@ -0,0 +1,7 @@
+echo "Launch vllm endpoint"
+nohup singularity run --nv \
+    --bind $GPFS_MODELS_REGISTRY_PATH:/data  \
+    $GPFS_VLLM_SINGULARITY  \
+    --model /data/$GPFS_VLLM_MODEL \
+    --host 0.0.0.0 \
+    --port 8080 $(echo $JOB_VLLM_PARAMS) > $JOB_PATH/logs/vllm.log 2>&1
