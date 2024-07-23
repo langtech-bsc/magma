c184a2163ca71573c815624add81b6430f628c17
Ankush Rana <ankush.rana@bsc.es>
2024-07-23 14:58:56 +0200

    update config

--- actions/addons/vlm.sh
+++ actions/addons/vlm.sh
@@ -1,1 +1,7 @@
-echo "Launching VLM"
+echo "Launch vllm endpoint"
+nohup singularity run --nv \
+    --bind $GPFS_MODELS_REGISTRY_PATH:/data  \
+    $GPFS_SINGULARITY_IMAGE_REGISTRY_PATH/vllm-inference.sif  \
+    --model /data/Mistral-7B-v0.1 \
+    --host 0.0.0.0 \
+    --port 8080 $(echo $JOB_TGI_PARAMS) > $JOB_PATH/logs/vllm.log 2>&1
