771616e96ca9436b3d62fb1f2a76f337c2f351e1
Ankush Rana <ankush.rana@bsc.es>
2024-07-23 15:15:01 +0200

    change tgi.sh

--- actions/addons/tgi.sh
+++ actions/addons/tgi.sh
@@ -2,8 +2,8 @@ if [ "$GPFS_VLLM" == "true" ]; then
     echo "Launch vllm endpoint"
     nohup singularity run --nv \
         --bind $GPFS_MODELS_REGISTRY_PATH:/data  \
-        $GPFS_SINGULARITY_IMAGE_REGISTRY_PATH/vllm-inference.sif  \
-        --model /data/Mistral-7B-v0.1 \
+        $GPFS_TGI_SINGULARITY  \
+        --model /data/$GPFS_TGI_MODEL \
         --host 0.0.0.0 \
         --port 8080 $(echo $JOB_TGI_PARAMS) > $JOB_PATH/logs/vllm.log 2>&1
 else
