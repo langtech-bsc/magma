3a0d81ca0275ec2b6b3c3e099a044971debe8c7d
Ankush <51945739+ankush13r@users.noreply.github.com>
2024-08-02 17:23:16 +0200

    Update vllm.sh

--- actions/addons/vllm.sh
+++ actions/addons/vllm.sh
@@ -10,10 +10,11 @@ else
     # MODEL_NAME remains unchanged in this case
 fi
 
-nohup singularity run -W / --nv \
+nohup singularity run --nv \
     --bind $GPFS_MODELS_REGISTRY_PATH:/$dir  \
     $GPFS_VLLM_SINGULARITY  \
-    --model $MODEL_NAME \
+    --model /$MODEL_NAME \
+    --served-model-name $GPFS_VLLM_MODEL
     --host 0.0.0.0 \
     --port 8080 \
     --tensor-parallel-size $SLURM_GPUS_ON_NODE $(echo $JOB_VLLM_PARAMS) > $JOB_PATH/logs/vllm.log 2>&1 &
