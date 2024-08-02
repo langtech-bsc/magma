695e71f2524a6db290cefd89cd3069579335afc4
Ankush <51945739+ankush13r@users.noreply.github.com>
2024-08-02 16:41:21 +0200

    Update vllm.sh

--- actions/addons/vllm.sh
+++ actions/addons/vllm.sh
@@ -1,8 +1,20 @@
 echo "Launch VLLM endpoint"
+MODEL_NAME=$GPFS_VLLM_MODEL
+
+MODEL_NAME="$GPFS_VLLM_MODEL"
+if [[ "$MODEL_NAME" != *"/"* ]]; then
+    dir="data"
+    MODEL_NAME="data/$MODEL_NAME"
+else
+    # Extract the directory part excluding the last segment
+    dir=$(dirname "$MODEL_NAME")
+    # MODEL_NAME remains unchanged in this case
+fi
+
 nohup singularity run --nv \
-    --bind $GPFS_MODELS_REGISTRY_PATH:/data  \
+    --bind $GPFS_MODELS_REGISTRY_PATH:/$dir  \
     $GPFS_VLLM_SINGULARITY  \
-    --model /data/$GPFS_VLLM_MODEL \
+    --model /$MODEL_NAME \
     --host 0.0.0.0 \
     --port 8080 \
     --tensor-parallel-size $SLURM_GPUS_ON_NODE $(echo $JOB_VLLM_PARAMS) > $JOB_PATH/logs/vllm.log 2>&1 &
