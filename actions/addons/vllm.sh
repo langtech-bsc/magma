8ed29a6bbd8a91ab3393308101a6364f2d011bac
PaulNdrei <18223023+PaulNdrei@users.noreply.github.com>
2024-08-02 17:07:17 +0200

    Update vllm.sh

--- actions/addons/vllm.sh
+++ actions/addons/vllm.sh
@@ -1,7 +1,6 @@
 echo "Launch VLLM endpoint"
-MODEL_NAME=$GPFS_VLLM_MODEL
-
 MODEL_NAME="$GPFS_VLLM_MODEL"
+
 if [[ "$MODEL_NAME" != *"/"* ]]; then
     dir="data"
     MODEL_NAME="data/$MODEL_NAME"
@@ -11,10 +10,10 @@ else
     # MODEL_NAME remains unchanged in this case
 fi
 
-nohup singularity run --nv \
+nohup singularity run -W / --nv \
     --bind $GPFS_MODELS_REGISTRY_PATH:/$dir  \
     $GPFS_VLLM_SINGULARITY  \
-    --model /$MODEL_NAME \
+    --model $MODEL_NAME \
     --host 0.0.0.0 \
     --port 8080 \
     --tensor-parallel-size $SLURM_GPUS_ON_NODE $(echo $JOB_VLLM_PARAMS) > $JOB_PATH/logs/vllm.log 2>&1 &
