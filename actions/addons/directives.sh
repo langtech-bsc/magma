c27810b5e4bea5cd04015c13881f33225546b25b
Ankush Rana <ankush.rana@bsc.es>
2024-08-06 14:46:35 +0200

    add slurm command from config.file

--- actions/addons/directives.sh
+++ actions/addons/directives.sh
@@ -1,3 +1,3 @@
-umask g+rwx
+umask g+rw
 module load singularity
 source %JOB_PATH%/job.env
--- src/launch.sh
+++
@@ -1,4 +0,0 @@
-
-# Use $JOB_LOGS_PATH save logs
-echo "Launch python script"
-singularity exec --nv --no-home --pwd /src --bind $JOB_PATH:/src --bind /gpfs:/gpfs $GPFS_SINGULARITY_IMAGE_REGISTRY_PATH/python-jupyter.sif python code/script.py > $JOB_LOGS_PATH/script.log 2>&1
