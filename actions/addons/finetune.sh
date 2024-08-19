44cd623a90005feec994be3d666486c9bbb04c45
Ankush Rana <ankush.rana@bsc.es>
2024-08-19 13:31:42 +0200

    add finetune

--- actions/addons/finetune.sh
+++ actions/addons/finetune.sh
@@ -1,5 +1,4 @@
-bash /gpfs/scratch/bsc88/virtual-environments/activate-fastchat.sh
-model_name=$JOB_OUTPUT_MODEL_NAME
+model_name=$JOB_FINETUNE_OUTPUT_MODEL_NAME
 
 export WANDB_PROJECT="instruction-tuning"
 export WANDB_NAME="${model_name}_${SLURM_JOBID}"
@@ -10,38 +9,29 @@ export WANDB_DIR=$PATH_RESULTS
 export WANDB_CONFIG_DIR=$WANDB_DIR/config
 mkdir -p $WANDB_DIR
 
-export GPUS_PER_NODE=$SLURM_GPUS #4
-export NNODES=$SLURM_NNODES
-export WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+export WORLD_SIZE=$SLURM_NTASKS
 export MASTER_PORT=29403
 head_node=$( scontrol show hostname ${SLURM_NODELIST} | head -n 1)
 head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
 export MASTER_ADDR=$head_node_ip
 
 
-
-export SLURM_CPU_BIND=none
-DIST_ARGS="
- --nproc_per_node=$GPUS_PER_NODE \
- --nnodes=$NNODES \
- --rdzv_id=$SLURM_JOB_ID \
- --rdzv_backend=c10d \
- --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT \
- "
+export SLURM_CPU_BIND=none # Required for mp.
 
 cat << EOF
 xxxxxxxxxxxxxxxxxxxxxxxxxxxx
-slurm cpu bind: $SLURM_CPU_BIND
-slurm cpu on node: $SLURM_CPUS_ON_NODE
-slurm gpu per node: $GPUS_PER_NODE
-slurm cpus per gpu: $SLURM_CPUS_PER_GPU
 master addr: ${MASTER_ADDR}
 master port: ${MASTER_PORT}
-num nodes: ${NNODES}
-DIST ARGS: $DIST_ARGS
+word size: ${WORLD_SIZE}
+num nodes: ${SLURM_NNODES}
 xxxxxxxxxxxxxxxxxxxxxxxxxxxx
 EOF
 
-srun torchrun $DIST_ARGS -m fastchat.train.train --output_dir $PATH_RESULTS \
+srun singularity exec --nv /gpfs/projects/bsc88/singularity-images/fastchat-pytorch.sif bash <<EOF
+export LOCAL_RANK=\$SLURM_LOCALID
+export RANK=\$SLURM_PROCID
+python -m fastchat.train.train \
     %FINETUNE_PARAMS%
-    --report_to 'wandb'
+    --output_dir $PATH_RESULTS \
+    --report_to wandb
+EOF
