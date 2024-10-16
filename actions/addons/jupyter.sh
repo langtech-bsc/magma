4c019fbe0227378798ba4e8c65dc6934c91c5624
Ankush <51945739+ankush13r@users.noreply.github.com>
2024-10-16 18:27:17 +0200

    Update jupyter.sh

--- actions/addons/jupyter.sh
+++ actions/addons/jupyter.sh
@@ -3,12 +3,10 @@ if [ -n "$GPFS_JUPYTER_WORKING_DIR" ]; then
     mkdir -p $GPFS_JUPYTER_WORKING_DIR
     BIND_WORKING_DIR="--bind $GPFS_JUPYTER_WORKING_DIR:/home/bsc/$USER/working_dir"
 fi
-source /gpfs/projects/bsc88/mlops/scripts/ienable.sh
-mkdir -p $TMPDIR/.local
+
 singularity exec --nv --no-home \
     --bind /gpfs:/gpfs \
     --bind $JOB_PATH:/home/bsc/$USER \
-    --bind $TMPDIR/.local:/home/bsc/$USER/.local \
     $BIND_WORKING_DIR \
     $GPFS_JUPYTER_SINGULARITY jupyter-lab \
     --notebook-dir=/home/bsc/$USER \
