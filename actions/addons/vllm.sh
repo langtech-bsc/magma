2a41f5e38bbedacea01286d42171bf18bd3b2718
Ankush <51945739+ankush13r@users.noreply.github.com>
2025-02-04 15:15:17 +0100

    Update vllm.sh

--- actions/addons/vllm.sh
+++ actions/addons/vllm.sh
@@ -53,6 +53,8 @@ else
     --bind $GPFS_MODELS_REGISTRY_PATH:/$dir \
     --env HOST_IP=$ip_addr \
     --env VLLM_HOST_IP=$ip_addr \
+    --env NCCL_SOCKET_IFNAME=eno1 \
+    --env GLOO_SOCKET_IFNAME=eno1 \
     $GPFS_VLLM_SINGULARITY ray start --block --head --port=$head_node_port &
 
     sleep 10
@@ -67,6 +69,8 @@ else
         --bind $GPFS_MODELS_REGISTRY_PATH:/$dir \
         --env HOST_IP=$worker_ip \
         --env VLLM_HOST_IP=$worker_ip \
+        --env NCCL_SOCKET_IFNAME=eno1 \
+        --env GLOO_SOCKET_IFNAME=eno1 \
         $GPFS_VLLM_SINGULARITY ray start --block --address $ip_head &
         
         sleep 5
