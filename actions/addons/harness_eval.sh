182a0029d3dc8c656b2959aa0db343154eb464a6
Ankush <51945739+ankush13r@users.noreply.github.com>
2025-03-27 14:12:52 +0100

    Update harness_eval.sh

--- actions/addons/harness_eval.sh
+++ actions/addons/harness_eval.sh
@@ -31,7 +31,7 @@ export MLFLOW_ENABLE_SYSTEM_METRICS_LOGGING="1"
 #export MLFLOW_EXPERIMENT_ID="0"
 
 singularity exec --nv $GPFS_HARNESS_EVAL_SINGULARITY bash <<EOF
-python -m lm_eval \
+accelerate launch -m lm_eval \
     --output_path ${OUTPUT_PATH} \
     --experiment_name ${MLFLOW_EXPERIMENT_NAME} \
     %HARNESS_EVAL_PARAMS%
